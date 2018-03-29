1d9c2e28103b117b9039abefb19f5b86fc7de4fc
Joe Schafer <joeschaferastula@gmail.com>
2018-03-29 13:27:06 -0500

    first day of tutorial

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,90 @@
+// Lecture: variables
+
+// var name = 'John';
+// console.log(name);
+
+// var lastName = 'Smith';
+// console.log(lastName);
+
+// var age = 26;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// Lecture: variables 2
+
+// var name = 'John';
+// var age = 26;
+
+// // console.log(name + age);
+// // console.log(age + age);
+
+// var job, isMarried;
+// // console.log(job);
+
+// job = 'teacher';
+// isMarried = false;
+
+// console.log(name + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried + '.');
+
+// age = 'thirty six';
+// job = 'driver';
+
+// console.log(name + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried + '.');
+
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+
+// alert(name + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried + '.');
+
+
+//Lecture: operators
+// var now = 2018;
+// var birthYear = now - 24;
+
+// birthYear = now - 24 * 2;
+// //2018 - 48
+// //1970
+// console.log(birthYear)
+
+// var ageJohn = 30;
+// var ageMark = 30;
+
+// ageJohn = ageMark = (3 + 5) * 4 - 6;
+// //ageJohn = ageMark = 26
+// //ageJohn = 26
+// ageJohn++;
+// ageMark *= 2;
+
+// console.log(ageJohn);
+// console.log(ageMark);
+
+
+//Lecture: if/else statements
+
+var name = 'John';
+var age = 26;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+  console.log(name + ' is married!');
+} else {
+  console.log(name + ' is not married.');
+}
+
+isMarried = false;
+// if(isMarried) {
+//   console.log('YES!');
+// } else {
+//   console.log('NO!');
+// }
+
+if(isMarried) {
+  console.log('YES!');
+}
+
+if (23 === "23") {
+  console.log('Something to print...');
+}
+
