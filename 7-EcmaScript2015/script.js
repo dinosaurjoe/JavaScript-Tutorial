b1516d24b975831fd70b42d0a986813d9244f306
Joe Schafer <joeschaferastula@gmail.com>
2018-05-08 15:16:45 -0500

    Arrow functions in ES6

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -85,32 +85,153 @@
 
 // Lecture: Strings in ES6
 
-let firstName = 'John';
-let lastName = 'Smith';
-const yearOfBirth = 1990;
+// let firstName = 'John';
+// let lastName = 'Smith';
+// const yearOfBirth = 1990;
 
-function calcAge(year) {
-  return 2018 - year;
+// function calcAge(year) {
+//   return 2018 - year;
+// }
+
+// //ES5
+
+// console.log('This is ' + firstName + ' ' + lastName +
+//  '. He was born in ' + yearOfBirth + ', Today he is ' +
+//  calcAge(yearOfBirth) + ' years old.');
+
+
+//  //ES6
+
+//  console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`);
+
+
+//  const n = `${firstName} ${lastName}`;
+
+//   console.log(n.startsWith('n'));
+//   console.log(n.endsWith('th'));
+//   console.log(n.includes(' '));
+//   console.log(`${firstName} `.repeat(5));
+
+
+// //Lecture: Arrow functions
+
+// const years = [1990, 1965, 1982, 1937];
+
+// // ES5
+
+// var ages5 = years.map(function(el){
+//     return 2018 - el;
+// });
+
+// console.log(ages5);
+
+
+// // ES6
+
+// let ages6 = years.map(el => 2018 - el);
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => `Age element ${index + 1}: ${2018 - el}`);
+
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => {
+//     const now = new Date().getFullYear();
+//     const age = now - el;
+
+//     return `Age element ${index + 1}: ${2018 - el}.`
+// });
+
+// console.log(ages6);
+
+// Arrow functions have lexical this variable
+
+// ES5
+
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+
+    var self = this;
+    document.querySelector('.green').addEventListener('click', function(){
+      var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+      alert(str);
+    })
+  }
 }
 
-//ES5
+// box5.clickMe();
 
-console.log('This is ' + firstName + ' ' + lastName +
- '. He was born in ' + yearOfBirth + ', Today he is ' +
- calcAge(yearOfBirth) + ' years old.');
+// EST 6
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+
+    document.querySelector('.green').addEventListener('click', () => {
+      var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+      alert(str);
+    })
+  }
+}
+
+box6.clickMe();
 
 
- //ES6
+// const box66 = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: () => {
 
- console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`);
+//     document.querySelector('.green').addEventListener('click', () => {
+//       var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+//       alert(str);
+//     })
+//   }
+// }
+
+// box66.clickMe();
 
 
- const n = `${firstName} ${lastName}`;
+function Person(name) {
+  this.name = name;
+}
 
-  console.log(n.startsWith('n'));
-  console.log(n.endsWith('th'));
-  console.log(n.includes(' '));
-  console.log(`${firstName} `.repeat(5));
+// ES5
+Person.prototype.myFriends5 =
+  function(friends) {
+
+    var arr = friends.map(function(el) {
+      return this.name + ' is friends with ' + el;
+    }.bind(this));
+
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+
+new Person('John').myFriends5(friends);
+
+// ES6
+
+Person.prototype.myFriends6 =
+  function(friends) {
+
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+
+new Person('Mike').myFriends6(friends);
+
+
+
+
+
+
 
 
 
