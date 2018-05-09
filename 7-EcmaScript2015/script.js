e1d76fe45b9af83dc9286284639bbb00536b58a0
Joe Schafer <joeschaferastula@gmail.com>
2018-05-09 11:51:38 -0500

    destructuring and arrays in es6

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -148,41 +148,26 @@
 
 // ES5
 
-var box5 = {
-  color: 'green',
-  position: 1,
-  clickMe: function() {
-
-    var self = this;
-    document.querySelector('.green').addEventListener('click', function(){
-      var str = 'This is box number ' + self.position + ' and it is ' + self.color;
-      alert(str);
-    })
-  }
-}
-
-// box5.clickMe();
-
-// EST 6
-const box6 = {
-  color: 'green',
-  position: 1,
-  clickMe: function() {
-
-    document.querySelector('.green').addEventListener('click', () => {
-      var str = 'This is box number ' + this.position + ' and it is ' + this.color;
-      alert(str);
-    })
-  }
-}
-
-box6.clickMe();
-
-
-// const box66 = {
+// var box5 = {
 //   color: 'green',
 //   position: 1,
-//   clickMe: () => {
+//   clickMe: function() {
+
+//     var self = this;
+//     document.querySelector('.green').addEventListener('click', function(){
+//       var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+//       alert(str);
+//     })
+//   }
+// }
+
+// // box5.clickMe();
+
+// // EST 6
+// const box6 = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: function() {
 
 //     document.querySelector('.green').addEventListener('click', () => {
 //       var str = 'This is box number ' + this.position + ' and it is ' + this.color;
@@ -191,41 +176,141 @@ box6.clickMe();
 //   }
 // }
 
-// box66.clickMe();
+// box6.clickMe();
 
 
-function Person(name) {
-  this.name = name;
+// // const box66 = {
+// //   color: 'green',
+// //   position: 1,
+// //   clickMe: () => {
+
+// //     document.querySelector('.green').addEventListener('click', () => {
+// //       var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+// //       alert(str);
+// //     })
+// //   }
+// // }
+
+// // box66.clickMe();
+
+
+// function Person(name) {
+//   this.name = name;
+// }
+
+// // ES5
+// Person.prototype.myFriends5 =
+//   function(friends) {
+
+//     var arr = friends.map(function(el) {
+//       return this.name + ' is friends with ' + el;
+//     }.bind(this));
+
+//     console.log(arr);
+// }
+
+// var friends = ['Bob', 'Jane', 'Mark'];
+
+// new Person('John').myFriends5(friends);
+
+// // ES6
+
+// Person.prototype.myFriends6 =
+//   function(friends) {
+
+//     var arr = friends.map(el => `${this.name} is friends with ${el}`);
+
+//     console.log(arr);
+// }
+
+// var friends = ['Bob', 'Jane', 'Mark'];
+
+// new Person('Mike').myFriends6(friends);
+
+// // Destructuring
+
+// //ES5
+// var john = ['John', 26];
+// //var name = john[0];
+// //var age = john[1];
+
+// //ES6
+
+// const [name, age] = ['John', 26];
+// console.log(name);
+// console.log(age);
+
+// const obj = {
+//   firstName: 'John',
+//   lastName: 'Smith'
+// };
+
+// const {firstName, lastName} = obj;
+// console.log(firstName);
+// console.log(lastName);
+
+// const {firstName: a, lastName: b} = obj;
+// console.log(a);
+// console.log(b);
+
+
+// function calcAgeRetirement(year) {
+//   const age = new Date().getFullYear() - year;
+//   return [age, 65 - age];
+// }
+
+
+// const [age2, retirement] = calcAgeRetirement(1990);
+// console.log(age2);
+// console.log(retirement);
+
+// Lecture: Arrays
+
+const boxes = document.querySelectorAll(".box");
+
+//ES5
+// var boxesArr5 = Array.prototype.slice.call(boxes);
+// boxesArr5.forEach(function(cur){
+//   cur.style.backgroundColor = 'dodgerblue';
+// });
+
+//ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+//ES5
+// for(var i = 0; i < boxesArr5.length; i++) {
+//   if(boxesArr5[i].className === 'box blue') {
+//     continue;
+//   }
+//   boxesArr5[i].textContent = 'I changed to blue';
+// }
+
+//ES6
+for (const cur of boxesArr6) {
+  if (cur.className.includes('blue')) {
+    continue;
+  }
+  cur.textContent = 'I changed to blue!';
 }
 
-// ES5
-Person.prototype.myFriends5 =
-  function(friends) {
+//ES5
 
-    var arr = friends.map(function(el) {
-      return this.name + ' is friends with ' + el;
-    }.bind(this));
+var ages = [12, 17, 8, 21, 14, 11];
 
-    console.log(arr);
-}
+var full = ages.map(function(cur) {
+  return cur >= 18;
+});
+console.log(full);
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
 
-var friends = ['Bob', 'Jane', 'Mark'];
 
-new Person('John').myFriends5(friends);
+//ES6
 
-// ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >=  18));
 
-Person.prototype.myFriends6 =
-  function(friends) {
-
-    var arr = friends.map(el => `${this.name} is friends with ${el}`);
-
-    console.log(arr);
-}
-
-var friends = ['Bob', 'Jane', 'Mark'];
-
-new Person('Mike').myFriends6(friends);
 
 
 
